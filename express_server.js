e01bf84838ab9397dce24f7e24b89c7b8760209b
Bastien Moy <moy.bastien@gmail.com>
2023-05-16 13:34:34 -0700

    adding random short URL generator and redirect to longURL

--- express_server.js
+++ express_server.js
@@ -5,6 +5,15 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -25,8 +34,15 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let randID = generateRandomString();
+  urlDatabase[randID] = req.body.longURL;
+  res.redirect(`/urls/${randID}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  console.log(longURL);
+  res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
